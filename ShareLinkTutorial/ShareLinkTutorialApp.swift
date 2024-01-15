69229b0d5bd618422f77b8b5bd9ab24269b2b14a
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-01-15 13:30:38 +0300

    Initial Commit

---
+++ ShareLinkTutorial/ShareLinkTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLinkTutorialApp.swift
+//  ShareLinkTutorial
+//
+//  Created by Fatih Durmaz on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShareLinkTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
